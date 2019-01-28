9f6ff6e511928874015f43272ba4a3b1fac4f992
Taylor Pechacek <taylortpechacek@gmail.com>
2019-01-28 12:58:30 +1100

    finished building the reusable input component and applied it to the new password field

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+            placeholder="user@gmail.com"
             label="Email"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text: text })}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email: email })}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry // Just listing this will equal true;
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password: password })}
+          />
+        </CardSection>
 
         <CardSection>
           <Button> Log in</Button>
