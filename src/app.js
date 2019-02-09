3e9868d919ee4461e78480c996a0854340f6ed67
Taylor Pechacek <taylortpechacek@gmail.com>
2019-02-09 12:15:11 +1100

    finished up wiring the different auth states to render content for the app. This is the end of this app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import {
+  Header,
+  Button,
+  CardSection,
+  Card,
+  Spinner
+} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     // firebase initalization.
     firebase.initializeApp({
@@ -15,13 +23,47 @@ class App extends Component {
       storageBucket: 'auth-react-native-cece2.appspot.com',
       messagingSenderId: '11977039500'
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      // logged in
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button
+                onPress={() => {
+                  firebase.auth().signOut();
+                }}
+              >
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      // logged out
+      case false:
+        return <LoginForm />;
+      // when null
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: 'white' }}>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
