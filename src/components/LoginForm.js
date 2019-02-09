0fe5152eb481505ecb4110485019d95aec224769
Taylor Pechacek <taylortpechacek@gmail.com>
2019-02-09 11:11:20 +1100

    wired up login form with auth and callbacks to firebase with error states

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      // the fail use case.
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -26,12 +47,22 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
         <CardSection>
-          <Button> Log in</Button>
+          <Button onPress={this.onButtonPress.bind(this)}> Log in</Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
